95a5905fa0edc7cbe51cfed6bc000ee655e8a57c
balajichennamshetty <clakshmipathibalaji811@gmail.com>
2024-03-19 19:33:52 +0530

    new

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -1,3 +1,4 @@
+#main code
 import tkinter as tk
 from tkinter import *
 import cv2
@@ -38,4 +39,210 @@ def manually_fill():
         ec.title('Warning!!')
         ec.configure(background='snow')
         Label(ec, text='Please enter your subject name!!!', fg='red',
+              bg='white', font=('times', 16, ' bold ')).pack()
+        Button(ec, text='OK', command=ec_delete, fg="black", bg="lawn green", width=9, height=1, activebackground="Red",
+               font=('times', 15, ' bold ')).place(x=90, y=50)
+
+    def fill_attendance():
+        ts = time.time()
+        Date = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d')
+        timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+        Time = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+        Hour, Minute, Second = timeStamp.split(":")
+        # Creatting csv of attendance
+
+        # Create table for Attendance
+        date_for_DB = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d')
+        global subb
+        subb = SUB_ENTRY.get()
+        DB_table_name = str(subb + "_" + Date + "_Time_" +
+                            Hour + "_" + Minute + "_" + Second)
+
+        import pymysql.connections
+
+        # Connect to the database
+        try:
+            global cursor
+            connection = pymysql.connect(
+                host='localhost', user='root', password='', db='manually_fill_attendance')
+            cursor = connection.cursor()
+        except Exception as e:
+            print(e)
+
+        sql = "CREATE TABLE " + DB_table_name + """
+                        (ID INT NOT NULL AUTO_INCREMENT,
+                         ENROLLMENT varchar(100) NOT NULL,
+                         NAME VARCHAR(50) NOT NULL,
+                         DATE VARCHAR(20) NOT NULL,
+                         TIME VARCHAR(20) NOT NULL,
+                             PRIMARY KEY (ID)
+                             );
+                        """
+
+        try:
+            cursor.execute(sql)  # for create a table
+        except Exception as ex:
+            print(ex)  #
+
+        if subb == '':
+            err_screen_for_subject()
+        else:
+            sb.destroy()
+            MFW = tk.Tk()
+            # MFW.iconbitmap('AMS.ico')
+            MFW.title("Manually attendance of " + str(subb))
+            MFW.geometry('880x470')
+            MFW.configure(background='grey80')
+
+            def del_errsc2():
+                errsc2.destroy()
+
+            def err_screen1():
+                global errsc2
+                errsc2 = tk.Tk()
+                errsc2.geometry('330x100')
+                # errsc2.iconbitmap('AMS.ico')
+                errsc2.title('Warning!!')
+                errsc2.configure(background='grey80')
+                Label(errsc2, text='Please enter Student & Enrollment!!!', fg='black', bg='white',
+                      font=('times', 16, ' bold ')).pack()
+                Button(errsc2, text='OK', command=del_errsc2, fg="black", bg="lawn green", width=9, height=1,
+                       activebackground="Red", font=('times', 15, ' bold ')).place(x=90, y=50)
+
+            def testVal(inStr, acttyp):
+                if acttyp == '1':  # insert
+                    if not inStr.isdigit():
+                        return False
+                return True
+
+            ENR = tk.Label(MFW, text="Enter Enrollment", width=15, height=2, fg="black", bg="grey",
+                           font=('times', 15))
+            ENR.place(x=30, y=100)
+
+            STU_NAME = tk.Label(MFW, text="Enter Student name", width=15, height=2, fg="black", bg="grey",
+                                font=('times', 15))
+            STU_NAME.place(x=30, y=200)
+
+            global ENR_ENTRY
+            ENR_ENTRY = tk.Entry(MFW, width=20, validate='key',
+                                 bg="white", fg="black", font=('times', 23))
+            ENR_ENTRY['validatecommand'] = (
+                ENR_ENTRY.register(testVal), '%P', '%d')
+            ENR_ENTRY.place(x=290, y=105)
+
+            def remove_enr():
+                ENR_ENTRY.delete(first=0, last=22)
+
+            STUDENT_ENTRY = tk.Entry(
+                MFW, width=20, bg="white", fg="black", font=('times', 23))
+            STUDENT_ENTRY.place(x=290, y=205)
+
+            def remove_student():
+                STUDENT_ENTRY.delete(first=0, last=22)
+
+            # get important variable
+            def enter_data_DB():
+                ENROLLMENT = ENR_ENTRY.get()
+                STUDENT = STUDENT_ENTRY.get()
+                if ENROLLMENT == '':
+                    err_screen1()
+                elif STUDENT == '':
+                    err_screen1()
+                else:
+                    time = datetime.datetime.fromtimestamp(
+                        ts).strftime('%H:%M:%S')
+                    Hour, Minute, Second = time.split(":")
+                    Insert_data = "INSERT INTO " + DB_table_name + \
+                        " (ID,ENROLLMENT,NAME,DATE,TIME) VALUES (0, %s, %s, %s,%s)"
+                    VALUES = (str(ENROLLMENT), str(
+                        STUDENT), str(Date), str(time))
+                    try:
+                        cursor.execute(Insert_data, VALUES)
+                    except Exception as e:
+                        print(e)
+                    ENR_ENTRY.delete(first=0, last=22)
+                    STUDENT_ENTRY.delete(first=0, last=22)
+
+            def create_csv():
+                import csv
+                cursor.execute("select * from " + DB_table_name + ";")
+                csv_name = 'C:/Users/Pragya singh/PycharmProjects/Attendace_management_system/Attendance/Manually Attendance/'+DB_table_name+'.csv'
+                with open(csv_name, "w") as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow(
+                        [i[0] for i in cursor.description])  # write headers
+                    csv_writer.writerows(cursor)
+                    O = "CSV created Successfully"
+                    Notifi.configure(text=O, bg="Green", fg="white",
+                                     width=33, font=('times', 19, 'bold'))
+                    Notifi.place(x=180, y=380)
+                import csv
+                import tkinter
+                root = tkinter.Tk()
+                root.title("Attendance of " + subb)
+                root.configure(background='grey80')
+                with open(csv_name, newline="") as file:
+                    reader = csv.reader(file)
+                    r = 0
+
+                    for col in reader:
+                        c = 0
+                        for row in col:
+                            # i've added some styling
+                            label = tkinter.Label(root, width=18, height=1, fg="black", font=('times', 13, ' bold '),
+                                                  bg="white", text=row, relief=tkinter.RIDGE)
+                            label.grid(row=r, column=c)
+                            c += 1
+                        r += 1
+                root.mainloop()
+
+            Notifi = tk.Label(MFW, text="CSV created Successfully", bg="Green", fg="white", width=33,
+                              height=2, font=('times', 19, 'bold'))
+
+            c1ear_enroll = tk.Button(MFW, text="Clear", command=remove_enr, fg="white", bg="black", width=10,
+                                     height=1,
+                                     activebackground="white", font=('times', 15, ' bold '))
+            c1ear_enroll.place(x=690, y=100)
+
+            c1ear_student = tk.Button(MFW, text="Clear", command=remove_student, fg="white", bg="black", width=10,
+                                      height=1,
+                                      activebackground="white", font=('times', 15, ' bold '))
+            c1ear_student.place(x=690, y=200)
+
+            DATA_SUB = tk.Button(MFW, text="Enter Data", command=enter_data_DB, fg="black", bg="SkyBlue1", width=20,
+                                 height=2,
+                                 activebackground="white", font=('times', 15, ' bold '))
+            DATA_SUB.place(x=170, y=300)
+
+            MAKE_CSV = tk.Button(MFW, text="Convert to CSV", command=create_csv, fg="black", bg="SkyBlue1", width=20,
+                                 height=2,
+                                 activebackground="white", font=('times', 15, ' bold '))
+            MAKE_CSV.place(x=570, y=300)
+
+            def attf():
+                import subprocess
+                subprocess.Popen(
+                    r'explorer /select,"C:\Users\Pragya Singh\PycharmProjects\Attendace_management_system\Attendance\Manually Attendance\-------Check atttendance-------"')
+
+            attf = tk.Button(MFW,  text="Check Sheets", command=attf, fg="white", bg="black",
+                             width=12, height=1, activebackground="white", font=('times', 14, ' bold '))
+            attf.place(x=730, y=410)
+
+            MFW.mainloop()
+
+    SUB = tk.Label(sb, text="Enter Subject : ", width=15, height=2,
+                   fg="black", bg="grey80", font=('times', 15, ' bold '))
+    SUB.place(x=30, y=100)
+
+    global SUB_ENTRY
+
+    SUB_ENTRY = tk.Entry(sb, width=20, bg="white",
+                         fg="black", font=('times', 23))
+    SUB_ENTRY.place(x=250, y=105)
+
+    fill_manual_attendance = tk.Button(sb, text="Fill Attendance", command=fill_attendance, fg="black", bg="SkyBlue1", width=20, height=2,
+                                       activebackground="white", font=('times', 15, ' bold '))
+    fill_manual_attendance.place(x=250, y=160)
+    sb.mainloop()
+
               
